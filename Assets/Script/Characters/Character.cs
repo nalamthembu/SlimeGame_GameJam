292f44e8b22c64f88b9aad506662099ea9bba193
Nala Mthembu <Nala00.12@gmail.com>
2023-05-11 08:15:32 +0200

    Blue Chuck Tailors

--- Assets/Script/Characters/Character.cs
+++ Assets/Script/Characters/Character.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float health { get; private set; }
+    public bool IsDead { get; private set; }
 }
